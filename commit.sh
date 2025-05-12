1ea3fdab36ec084c5cff7491b70577f36fe74940
Yudi Yuswita Sunarto <yudi.sunarto@accelbyte.net>
2025-05-12 11:10:40 +0700

    bootcamp self - day 2

--- commit.sh
+++ commit.sh
@@ -11,7 +11,7 @@ commit_prefix="bootcamp self" # Prefix for your commit message
 
 # Get the current day number (you'll need to manually update this for each day)
 # **IMPORTANT: You will need to edit the 'current_day' variable below for each day.**
-current_day="4"
+read -p "Enter the current bootcamp day: " current_day
 
 # Construct the commit message
 commit_message="${commit_prefix} - day ${current_day}"
